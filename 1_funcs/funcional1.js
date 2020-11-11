10f648bb9b1c3606f223291e5faf0cf28089629a
manuel.betancurt <manuel.betancurt@sca.com.au>
2020-11-11 16:25:20 +1100

    func js

--- 1_funcs/funcional1.js
+++ 1_funcs/funcional1.js
@@ -4,3 +4,42 @@ function suma (a, b){
 }
 
 console.log("sua suma::", suma(2,3))
+
+function multiplica (a, b){
+    return a * b
+}
+
+console.log("sua multiplik::", multiplica(2,3))
+
+//first class functions
+// 1. pass around
+// 2. can be assigned to variables
+// 3. stored in more complex data structures
+
+const operations = {
+    suma,
+    multiplica
+}
+
+console.log ("sum of first class::", operations.suma(2,3))
+console.log ("mult of first class::", operations.multiplica(2,3))
+
+//
+//Higher order functions
+
+const numbers = [1, 1, 2, 3, 5, 8]
+const transformFunction = x => x + 2
+
+console.log ("originale    :: ", numbers)
+console.log ("transformatio:: ", numbers.map(transformFunction))
+
+// return func from other func
+
+const makeGreeter = greeting => name => `${greeting}, ${name}!`
+
+const greet = makeGreeter('haho')
+console.log(greet('mako'))
+
+
+//notes
+//practice: map, reduce, filter, compose, forEach, etc
