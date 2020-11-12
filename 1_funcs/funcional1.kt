f8ce0926115a2c6cd80a8218be0c2bb2402ca04a
manuel.betancurt <manuel.betancurt@sca.com.au>
2020-11-12 17:09:26 +1100

    functions inside const, swift and kotlin not working yet, polishing

--- 1_funcs/funcional1.kt
+++ 1_funcs/funcional1.kt
@@ -2,6 +2,23 @@ fun suma(a: Int, b: Int): Int{
     return a + b
 }
 
+fun multiplica (a: Int, b: Int): Int{
+    return a * b
+}
+
+const operations = {
+    suma()
+    multiplica()
+}
+
 fun main() {
     println("sua suma:: ${suma(2, 3)}")
+    println("sua multiplica:: ${multiplica(2, 3)}")
+
+    println("sua multiplica:: ${operations.multiplica(2,3)}")
+
 }
+
+//12 nov
+//blocked, copy from jS example
+//https://stackoverflow.com/questions/64798614/kotlin-functions-inside-a-const
--- 1_funcs/funcional1_ios.playground/Contents.swift
+++ 1_funcs/funcional1_ios.playground/Contents.swift
@@ -6,4 +6,21 @@ func suma(a:Int, b:Int) -> Int {
     return a + b
 }
 
+func multiplica(a:Int, b:Int) -> Int {
+    return a * b
+}
+
 print("sua suma", suma(a: 2, b: 3))
+print("sua multiplica", multiplica(a: 2, b: 3))
+
+var operations = {
+    suma
+    multiplica
+}
+
+
+print("sua operatio", operations.suma(a: 2, b: 3)
+
+//12 nov
+//blocked, copy from jS example
+//    https://stackoverflow.com/questions/64798744/swift-functions-inside-a-var
